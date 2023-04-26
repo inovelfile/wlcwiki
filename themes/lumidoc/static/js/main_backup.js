ed34af8647ddcfa103c9b8b89eba236888873c52
Iyan Sanjaya <iyansanjaya1@gmail.com>
2023-04-26 23:40:53 +0700

    update

--- themes/lumidoc/static/js/main_backup.js
+++ themes/lumidoc/static/js/main_backup.js
@@ -1,4 +1,4 @@
-// SLIDENAV
+// SLIDENAV START
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
